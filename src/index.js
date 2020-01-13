9aec6e65558d74ba0418480632c119b763787798
Imran <thanksimran@gmail.com>
2020-01-13 10:38:16 +0000

    started the output management chapter of the webpack guides

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+import _ from "lodash";
+import printMe from "./print";
+import "./styles.scss";
+
+function Component() {
+  const element = document.createElement("div");
+
+  element.innerHTML = _.join(["Hello", "Webpack"], " ");
+  element.classList.add("hello");
+  return element;
+}
+
+document.body.appendChild(Component());
+printMe();
